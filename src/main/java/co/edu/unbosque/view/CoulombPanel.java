475a6032fd0dceafb1f01dca548947052311d714
Tyreh <oscar7u72@gmail.com>
2022-03-06 18:56:43 -0500

    Corrections in code.

--- src/main/java/co/edu/unbosque/view/CoulombPanel.java
+++ src/main/java/co/edu/unbosque/view/CoulombPanel.java
@@ -5,23 +5,16 @@ import java.awt.*;
 
 public class CoulombPanel extends JPanel {
 
-    private final String[] fieldLabels = new String[]{"Carga 1 (q1): ", "Carga 2 (q2): ", "Distancia: (m): ", "Fuerza Eléctrica: "};
-    private final String[] prefixList = new String[]{"Newton", "Millinewton", "Micronewton", "Nanonewton", "Piconewton"};
+    private final String[] fieldLabels = new String[]{"Carga 1 (q1): ", "Carga 2 (q2): ", "Distancia: (m): "};
+    private final String[] prefixList = new String[]{"Micro", "Pico", "Nano"};
     private final FormattedComponents[] formattedTextFields = new FormattedComponents[fieldLabels.length];
     private final FormattedComponents prefixes = new FormattedComponents("Select a unit: ", prefixList);
 
     public CoulombPanel() {
-        setLayout(new GridLayout(1,0,0,0));
+        setLayout(new BorderLayout());
         setVisible(true);
 
-        add(rightPanel());
-    }
-
-
-    public JPanel rightPanel() {
-        final JPanel panel = new JPanel(new BorderLayout());
-        panel.add(initFields(), BorderLayout.CENTER);
-        return panel;
+        add(initFields(), BorderLayout.CENTER);
     }
 
     public JPanel initFields() {
@@ -31,7 +24,6 @@ public class CoulombPanel extends JPanel {
             formattedTextFields[i] = new FormattedComponents(fieldLabels[i], 20);
             panel.add(formattedTextFields[i]);
         }
-        formattedTextFields[3].getTextField().setEnabled(false);
         return panel;
     }
 
